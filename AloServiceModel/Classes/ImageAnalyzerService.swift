f0e5e5bef57dbeec4a3ea7109f9515020a80386e
aloisiofjrdev <aloisiofjr@hotmail.com>
2021-01-14 21:26:33 -0300

    big bang2

--- AloServiceModel/Classes/ImageAnalyzerService.swift
+++ AloServiceModel/Classes/ImageAnalyzerService.swift
@@ -15,6 +15,9 @@ public protocol ImageAnalyzerService {
 
 public class ImageAnalyzer: ImageAnalyzerService {
     
+    public init() {
+    }
+    
     public func analyzeImage(image: UIImage, navigationController: UINavigationController, failure: @escaping (_ error: String) -> Void) {
         
         guard let ciImage = CIImage(image: image) else {
